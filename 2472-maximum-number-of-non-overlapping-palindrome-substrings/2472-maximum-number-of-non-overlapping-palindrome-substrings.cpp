49eb46d850a50cbc5a5b59bd9bf0c08043662667
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-11-13 14:24:16 +0530

    Time: 985 ms (11.11%), Space: 227.8 MB (11.11%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,32 +1,26 @@
 class Solution {
+private:
+    bool isPal(int start, int end,string &s,vector<vector<int>>&dp){
+        if(start>end){
+            return 1;
+        }
+        if(dp[start][end]!= -1){
+            return dp[start][end];
+        }
+        if(s[start]!=s[end]){
+            return 0;
+        }
+        
+      return dp[start][end] = isPal(start+1,end-1,s,dp);
+    }
 public:
     int maxPalindromes(string s, int k) {
          int n =s.length();
-     vector<vector<int>>dp(n,vector<int>(n,0));
+     vector<vector<int>>dp(n,vector<int>(n,-1));
         int row=0;
         int col=0;
    
-for(int i=0;i<n;i++){
-    int l=0;
-    int r= i;
-    while(r<n){
-        if(i==0){
-            dp[l][r]=1;
-        }
-        else if(i==1){
-            if(s[l]==s[r]){
-                 dp[l][r]=1;
-            }
-        }
-        else{
-            if(s[l]==s[r] && dp[l+1][r-1]==1){
-                dp[l][r]=1;
-            }
-        }
-        l++;
-        r++;
-    }
-    }
+
         
         vector<int>dp2(n+1,0);
         for(int i=k;i<=n;i++){
@@ -34,7 +28,7 @@ for(int i=0;i<n;i++){
             for(int j=i-(k-1);j>=1;j--){
                 // cout<<i<<" "<<j<<"\n";
                 
-                if(dp[j-1][i-1]==1){
+                if(isPal(j-1,i-1,s,dp)==1){
                     dp2[i] = max(dp2[j-1]+1,dp2[i]);
                 }
                
