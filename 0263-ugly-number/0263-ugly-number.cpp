9b3777f3076626848d0ba5d711cadc71570371c5
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-11-18 13:24:32 +0530

    Time: 8 ms (11.77%), Space: 5.9 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n==0){
-return false;}
-      while(n!=1){
-          if(n%2==0){
-              n=n/2;
-          }
-         else if(n%3==0){
-n=n/3;
-          }
-         else if(n%5==0){
-n=n/5;}
-          else if(n==1){
-                return true;
-          }
-          else{
-              return false;
-          }
-      }
-        return true;
+        
+        if(n == 0){
+            return false;
+        }
+        
+        for(int k =2; k <= 5; k++){
+            while(n%k==0){
+                n = n/k;
+            }
+        }
+        
+        return n==1;
     }
 };
