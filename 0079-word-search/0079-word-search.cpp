a28df62783026a361d6f1cbbd2941ce2ec65c80b
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-11-24 11:00:11 +0530

    Time: 528 ms (63.66%), Space: 8.2 MB (17.64%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,53 +1,48 @@
 class Solution {
 private:
-    bool findWord(vector<vector<char>>& board, string &word, int index,int row, int col, int numRows, int numCols,vector<vector<int>>&visited){
+    bool findWord(vector<vector<char>>& board,vector<vector<int>>&visited,string &word, int row,int col,int len, int index){
         
-          if(index == word.size()){
+        if(index==len){
             return true;
         }
-
-        if(row<0 || col<0 || row>=numRows || col>=numCols || (visited[row][col] == 1)){
+        if(row<0 || col<0 || row>=board.size() || col>=board[0].size() || word[index] != board[row][col] || visited[row][col] == 1){
             return false;
         }
-        
-        if((word[index] != board[row][col])){
-            return false;
-        }
-
         visited[row][col] = 1;
         
-        int dir1 = findWord(board, word, index+1, row+1, col, numRows, numCols, visited);
-        int dir2 = findWord(board, word, index+1, row-1, col, numRows, numCols, visited);
-        int dir3 = findWord(board, word, index+1, row, col+1,numRows, numCols, visited);
-        int dir4 = findWord(board, word, index+1, row, col-1, numRows, numCols, visited);
-
-        if(dir1 || dir2 || dir3 || dir4){
-            return true;
-        }
-
+        bool p1 = findWord(board,visited,word,row+1,col,len,index+1);
+        bool p2 = findWord(board,visited,word,row-1,col,len,index+1);
+        bool p3 = findWord(board,visited,word,row,col+1,len,index+1);
+        bool p4 = findWord(board,visited,word,row,col-1,len,index+1);
+        
         visited[row][col] = 0;
-        return false;
+        
+        return p1 || p2 || p3 || p4;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        
+        int len = word.size();
+        
         int numRows = board.size();
-        int numColumns = board[0].size();
-
-        for(int row = 0; row < numRows; ++row){
-
-            for(int col = 0; col<numColumns; ++col){
-            cout<<row<<" "<<col<<"\n";
+        int numCols = board[0].size();
+        
+        for(int row = 0;row<numRows;row++){
+            for(int col = 0;col<numCols;col++){
                 if(board[row][col] == word[0]){
-                      vector<vector<int>>visited(numRows,vector<int>(numColumns,0));
-                bool isExist = findWord(board, word,0,row,col,numRows,numColumns,visited);
-                if(isExist){
-                    // cout<<"ihihiiihi";
-                    return true;
+                    
+                    vector<vector<int>>visited(numRows,vector<int>(numCols,0));
+                    bool isPresent  = findWord(board,visited,word,row,col,len,0);
+                    
+                    if(isPresent){
+                        return true;
+                    }
                 }
-                }
+                
             }
         }
-
+        
+        
         return false;
     }
 };
