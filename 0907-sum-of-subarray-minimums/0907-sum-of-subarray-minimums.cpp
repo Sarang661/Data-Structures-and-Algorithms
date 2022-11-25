ee77e2dbb000caf42c55798fa51ea18396ce2618
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-11-25 10:45:02 +0530

    Time: 258 ms (31.25%), Space: 46.9 MB (6.35%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -3,24 +3,28 @@ public:
     int sumSubarrayMins(vector<int>& arr) {
     
         
-                long long len = arr.size();
+        long long len = arr.size();
         long long ans = 0;
-               long long mod = 1e9 + 7;
+        long long mod = 1e9 + 7;
         
         vector<long long>left(len);
-        vector<        long long>right(len);
-        stack<        long long>st;
+        vector<long long>right(len);
+        stack<long long>st;
         
         for(int i=0;i<len;i++){
+            
             while(!st.empty() && arr[st.top()]>arr[i]){
                 st.pop();
             }
+            
+            
             if(st.empty()){
                  left[i] = i+1;
             }
             else{
             left[i] = i-st.top();
             }
+            
             
             st.push(i);
         }
@@ -30,15 +34,18 @@ public:
         }
         
          for(int i=0;i<len;i++){
+             
             while(!st.empty() && arr[st.top()]>arr[i]){
                   right[st.top()] = i-st.top();
                 st.pop();
             }
            
+             
             st.push(i);
         }
         
         while(!st.empty()){
+            
             right[st.top()] = len - st.top();
             st.pop();
         }
