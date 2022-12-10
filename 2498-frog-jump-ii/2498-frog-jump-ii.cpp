0db72d8f0adf708a79bcb4ab1c6525747f88e978
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-12-10 21:40:41 +0530

    Time: 967 ms (11.11%), Space: 136.4 MB (22.22%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -0,0 +1,69 @@
+class Solution {
+private:
+    bool poss(int val,vector<int>v){
+        
+        int n = v.size();
+        int prev = 0;
+        bool flag = false;
+        for(int i=1;i<n;i++){
+            if((v[i]-prev)<=val){
+                flag = true;
+              
+            }
+               else{
+                    // cout<<i<<" ";
+                   if(flag == false){
+                       return false;
+                   }
+                   
+                   prev = v[i-1];
+                   
+                   if((i-1)!= 0 && (i-1)!=(n-1)){
+                            
+                   v[i-1] = -1;
+                   }
+            
+               }
+               
+       }
+        
+        prev = v[n-1];
+        
+  
+             for(int i=n-1;i>=0;i--){
+            if(v[i]!= -1 && abs(v[i]-prev)<=val){
+               
+              prev = v[i];
+            }
+              
+       }
+        
+        return prev == 0;
+        
+    }
+public:
+    int maxJump(vector<int>& s) {
+        
+        int n = s.size();
+        int low = s[n-1] - s[n-2];
+        
+        int high = s[n-1] - s[0];
+        int ans = -1;
+        
+        while(low<high){
+            int m = low + (high-low)/2;
+            // cout<<m<<" ";
+            if(poss(m,s)){
+               
+                ans = m;
+                high = m;
+                
+            }
+            else{
+                 low = m+1;
+            }
+        }
+        
+        return low;
+    }
+};
