276b395b2afc99265726a6de6cf0a39d8a63969a
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-12-12 18:33:13 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (29.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,17 +5,20 @@ public:
         // n = 1  1  n= 2  0th or  1st   n = 3     1st 2nd  number of ways to get up to 1st step + number of ways to get u to 3rd step
         
         vector<int>dp(n+1,0);
-        
-        dp[0] = 1;
-        
-    dp[1]  = 1;
+   
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = 1;
         
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+          curr = prev1 + prev2;
+            
+            prev1 = prev2;
+            prev2 = curr;
         }
         
         
         
-        return dp[n];
+        return curr;
     }
 };
