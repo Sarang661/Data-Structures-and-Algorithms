083c2ad1184c84eec9425d7040a43f9fc51c534f
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-12-12 18:33:26 +0530

    Time: 3 ms (34.80%), Space: 6.2 MB (29.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // 1 2  
-        // n = 1  1  n= 2  0th or  1st   n = 3     1st 2nd  number of ways to get up to 1st step + number of ways to get u to 3rd step
+      
         
         vector<int>dp(n+1,0);
    
