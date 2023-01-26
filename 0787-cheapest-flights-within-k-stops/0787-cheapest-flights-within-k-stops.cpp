10edc40ad2d9bd40762548cac4298c709554d638
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2023-01-26 16:58:00 +0530

    Time: 37 ms (52.40%), Space: 16.1 MB (16.28%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,48 +2,44 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        vector<int>dis(n,1e9);
-        
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>q;
         vector<vector<int>>adj[n];
         
         for(int i=0;i<flights.size();i++){
             adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
         }
+        
+        vector<int>dist(n,1e9);
+        
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>q;
+        
         q.push({0,0,src});
         
-        dis[src]  = 0;
-        
-        while(q.empty()==false){
-            
-            int numStops = q.top()[0];
-            int distance = q.top()[1];
+        dist[src] = 0;
+        while(q.empty()== false){
+            int stops = q.top()[0];
+            int val = q.top()[1];
             int node = q.top()[2];
             q.pop();
-            
-            // cout<<numStops<<" "<<distance<<" "<<node<<"\n";
+            if(stops > k){
+                continue;
+            }
             for(auto num:adj[node]){
-                
-                int dist = num[1];
                 int node2 = num[0];
-     
-                if(dis[node2] > dist + distance && numStops < (k+1)){
-                    
-                    if(numStops+1 == (k+1) && node2!=dst){
-continue;}
-                    dis[node2] = dist + distance;
-                    // cout<<numStops<<" "<<node2<<" "<<dis[node2]<<"\n";
-                    q.push({numStops+1,dis[node2],node2});
+                int val2 = num[1];
+                
+                if(dist[node2] > val + val2){
+                       
+                    dist[node2] = val + val2;
+                    q.push({stops+1,dist[node2], node2});
                 }
             }
         }
-                
-       
         
-        if(dis[dst]==1e9){
+        if(dist[dst] == 1e9){
             return -1;
         }
-
-        return dis[dst];
+        
+        return dist[dst];
+        
     }
 };
