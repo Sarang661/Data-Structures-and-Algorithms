f050b76fcc39ab62d702b509eddd2aac9f8744d6
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2023-02-08 09:03:05 +0530

    Time: 513 ms (21.03%), Space: 17.3 MB (29.40%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,23 +4,23 @@ public:
         
         int n = nums.size();
         
-        vector<int>dp(n, 1e9);
-        
+        vector<int>dp(n);
         dp[n-1] = 0;
-        
+
         for(int index = n-2; index >= 0; index--){
             
-            for(int jump = 0; jump <= nums[index]; jump++){
-                
-                if(index+jump < n){
-                 
-                dp[index] = min(1+dp[index+jump], dp[index]);
-                    
-                    }
-            }
-        }
+                     
+            dp[index] = n;
         
-     
+            for(int j= 1; j <= nums[index]; j++){
+            
+                int curr = min(n-1, index+j);
+                
+                dp[index] = min(dp[index], dp[curr]+1);
+
+              
+            }          
+        }
         
         return dp[0];
     }
