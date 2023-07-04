ec61cf9418d734d0bffc5494799405c9a97c47c2
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2023-07-05 05:01:01 +0530

    Time: 9 ms (58.24%), Space: 9.5 MB (41.19%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -8,9 +8,17 @@ public:
             oneCount=0;
             for(int j=0;j<n;j++){
                 if((nums[j]&(1<<i))){
+                if(nums[j] == -4){
+                    cout<<1<<" ";
+                }
+                    // cout<<nums[j]<<" "<<(nums[j]&(1<<i))<<"\n";
                     oneCount++;
                 }
+                else if(nums[j] == -4){
+                    cout<<0<<" ";
+                }
             }
+            
             if((oneCount%3)){
                 ans=ans | (1<<i);
             }
