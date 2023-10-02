847ca75cc115624ca37add5945381d83ccc2e80b
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2023-10-02 18:14:48 +0530

    Time: 17 ms (46.77%), Space: 10.2 MB (50.31%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        int n = s.size();
+        int i = 0;
+        int j = 0;
         
-        
-        int prev = 0;
-        
-        for(int index = 0; index <= s.size(); index++){
-            
-            if(s[index] == ' ' || index == (s.size())){
-                
-
-                reverse(s.begin()+prev, s.begin()+ index);
-                prev = index+1;
+        while(j<n){
+            if(s[j]==' ' || j==n-1){
+                int k = j;
+                if(s[j]==' '){
+                j--;
+                }
+                while(i<j){
+                    swap(s[i],s[j]);
+                    i++;
+                    j--;
+                }
+                j=k+1;
+                i=k+1;
             }
+            j++;
         }
-        
         return s;
     }
 };
